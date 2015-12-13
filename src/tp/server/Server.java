9dc30b1b0330d7c11e836cf9fece2852ee1c791e
Pyhro36 <pylou_11@hotmail.fr>
2015-12-13 15:41:53 +0100

    ajout server

---
+++ src/tp/server/Server.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package tp.server;
+
+import java.util.LinkedList;
+
+import tp.protocol.Message;
+
+/**
+ * @author Pierre-Louis
+ *
+ */
+public class Server {
+
+	private LinkedList<Message> history;
+	private LinkedList<String> pseudoClients;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+	
+	public void 
+
+}
