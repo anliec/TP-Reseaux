8b8d668ae040368ea93f1eaea79b3f7fe99b51e0
Pyhro36 <pylou_11@hotmail.fr>
2015-12-13 16:56:15 +0100

    server

--- src/tp/server/Request.java
+++ src/tp/server/Request.java
@@ -4,11 +4,7 @@
 package tp.server;
 
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.util.LinkedList;
 
-import tp.protocol.ReceptionItf;
 import tp.protocol.Message;
 import tp.protocol.RequestItf;
 
@@ -18,56 +14,38 @@ import tp.protocol.RequestItf;
  */
 public class Request implements RequestItf {
 	
-	private LinkedList<Message> history;
-	private LinkedList<String> pseudoClients;
+	private Server server;
+	
 	/**
 	 * 
 	 */
-	public Request() {
+	public Request(Server server) {
 		// TODO Auto-generated constructor stub
+		this.server = server;
 	}
 
 	/* (non-Javadoc)
 	 * @see tp.protocol.RequestItf#send(tp.protocol.Message)
 	 */
-	@Override
 	public void send(Message aMessage) throws RemoteException {
 		// TODO Auto-generated method stub
-		history.add(aMessage);
-		
-		for(int i = 0; i < pseudoClients.size(); i++) {
-			
-			try {
-	            
-	        	Registry registry = LocateRegistry.getRegistry();
-	            ReceptionItf stub = (ReceptionItf) registry.lookup(pseudoClients.get(i));
-	            stub.receive(aMessage);
-	            
-	        } catch (Exception e) {
-	            
-	        	System.err.println("Server exception : Client : " + pseudoClients.get(i) + " not found ");
-	        	logout(pseudoClients.get(i));
-	            e.printStackTrace();
-	        }
-		}
+		server.addMessage(aMessage);
 	}
 
 	/* (non-Javadoc)
 	 * @see tp.protocol.RequestItf#login(java.lang.String)
 	 */
-	@Override
 	public int login(String pseudo) throws RemoteException {
 		// TODO Auto-generated method stub
-		return 1;
+		return server.addClient(pseudo);
 	}
 
 	/* (non-Javadoc)
 	 * @see tp.protocol.RequestItf#logout(long)
 	 */
-	@Override
 	public int logout(String pseudo) throws RemoteException {
 		// TODO Auto-generated method stub
-		return 1;
+		return server.removeClient(pseudo);
 	}
 
 }
--- src/tp/server/Server.java
+++ src/tp/server/Server.java
@@ -3,18 +3,54 @@
  */
 package tp.server;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 import tp.protocol.Message;
+import tp.protocol.ReceptionItf;
+import tp.protocol.RequestItf;
 
 /**
  * @author Pierre-Louis
- *
+ * Classe du server
  */
+
 public class Server {
 
-	private LinkedList<Message> history;
-	private LinkedList<String> pseudoClients;
+	private LinkedList<Message> history; //plus optimise en ajout/suppression
+	private ArrayList<String> pseudoClients; //plus optimise en acces
+	
+	/**
+	 * 
+	 */
+	public Server() { 
+		
+		history = new LinkedList<Message>();
+		pseudoClients = new ArrayList<String>();
+		
+		try {
+			
+        	LocateRegistry.createRegistry(1099); //lance le registre
+            Request obj = new Request(this);
+            RequestItf stub = (RequestItf) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("Request1", stub);
+
+            System.err.println("Server ready");
+			
+        } catch (Exception e) {
+        	
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+		
+	}
 	
 	/**
 	 * @param args
@@ -22,10 +58,76 @@ public class Server {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Server server = new Server();
+	}
+	/**
+	 * 
+	 * @param aMessage
+	 */
+	public void addMessage(Message aMessage) {
 		
-
+		history.add(aMessage);
+		
+		for(int i = 0; i < pseudoClients.size(); i++) {
+		
+			try {
+	            
+	        	Registry registry = LocateRegistry.getRegistry();
+	            ReceptionItf stub = (ReceptionItf) registry.lookup(pseudoClients.get(i));
+	            stub.receive(aMessage);
+	            
+	        } catch (Exception e) {
+	            
+	        	System.err.println("Server exception : Client : " + pseudoClients.get(i) + " not found ");
+	        	removeClient(pseudoClients.get(i));
+	            e.printStackTrace();
+	        }
+		}
+	}
+	/**
+	 * Methode d'ajout d'un client au serveur
+	 * @param pseudo
+	 * @return 1 si le client a ete correctement integre, 0 sinon, lorsque le pseudo est deja pris
+	 */
+	public int addClient(String pseudo) {
+		
+		ListIterator<String> li = pseudoClients.listIterator();
+		while ( li.hasNext() && !li.next().equals(pseudo) );
+		
+		if(!li.hasNext()) {
+			
+			pseudoClients.add(pseudo);
+			return 1;
+		
+		} else {
+			
+			System.err.println("pseudo " + pseudo + " already used");
+			return 0;
+		}
 	}
 	
-	public void 
+	/**
+	 * Methode de suppression d'un client du server
+	 * @param pseudo le pseudo du client
+	 * @return 1 si le client etait bien connecte au serveur et a donc bien ete retire, 0 sinon
+	 */
+	
+	public int removeClient(String pseudo) {
+		
+		ListIterator<String> li = pseudoClients.listIterator();
+		while ( li.hasNext() && !li.next().equals(pseudo) );
+		
+		if(li.hasNext()) {
+			
+			pseudoClients.remove(pseudo);
+			return 1;
+		
+		} else {
+			
+			System.err.println("client " + pseudo + " not in server");
+			return 0;
+		}
+		
+	}
 
 }
